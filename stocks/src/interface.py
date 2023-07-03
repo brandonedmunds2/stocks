192903cea19dbb5bcdff00b3d8560cba353730ab
Brandon Edmunds <brandonedmunds2@gmail.com>
2023-07-03 14:32:29 -0400

    .

---
+++ stocks/src/interface.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="stocks"
+    )
+    parser.add_argument("-t", "--ticker_directory_path", type=str,
+                        help="Path for ticker symbols directory", default="./tickers")
+    parser.add_argument(
+        "-o",
+        "--output_directory_path",
+        type=str,
+        default="./results",
+        help="Path of the output directory (creates directory if not present)",
+    )
+    args = parser.parse_args()
+    return args
+
+def setup_env(args):
+    if not os.path.exists(args.output_directory_path):
+        os.makedirs(args.output_directory_path)
---
+++ stocks/src/stocks.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import os
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import interface
+
+def load_tickers(args):
+    dfs=[]
+    for f in os.listdir(args.ticker_directory_path):
+        dfs.append(pd.read_csv(os.path.join(args.ticker_directory_path,f)))
+    tickers=pd.concat(dfs).iloc[:,0]
+    tickers=tickers.dropna().values
+    return tickers
+
+
+def annual_perc(tickers):
+    end = datetime.now()
+    end1 = end - timedelta(days=3)
+    start = end - timedelta(weeks=52)
+    start1 = start + timedelta(days=3)
+    start_data = yf.download(' '.join(tickers), start=start, end=start1).iloc[0,:]['Close']
+    end_data = yf.download(' '.join(tickers), start=end1, end=end).iloc[-1,:]['Close']
+    perc=end_data/start_data
+    return perc
+
+def main():
+    args = interface.get_args()
+    interface.setup_env(args)
+    tickers=load_tickers(args)
+    perc=annual_perc(tickers)
+    perc.sort_values(ascending=False)[:20].plot.bar()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
